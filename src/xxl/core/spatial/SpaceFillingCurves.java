6b5aae01aec6090729f6dfba9f39d2df45df4dcb
daniar.ac@googlemail.com <daniar.ac@googlemail.com@6cdef95d-3fa7-774b-3207-c1bd5d6c1983>
2011-07-15 07:53:01 +0000

    Added new Z-Curve functions, such as next in box, which computes the next value of Z-Curve in the query box and One for generating all the ranges of the z-curve which are within the query box.

--- src/xxl/core/spatial/SpaceFillingCurves.java
+++ src/xxl/core/spatial/SpaceFillingCurves.java
@@ -24,6 +24,11 @@ License along with this library;  If not, see <http://www.gnu.org/licenses/>.
 */
 package xxl.core.spatial;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+
 import xxl.core.math.Maths;
 import xxl.core.spatial.rectangles.Rectangle;
 import xxl.core.util.BitSet;
@@ -302,4 +307,285 @@ public class SpaceFillingCurves {
 		return zCode(bitField, minLevel);
 	}
 
+	
+
+	/**
+	 * 
+	 * @param zPoint
+	 * @param lPoint
+	 * @param rPoint
+	 * @return
+	 */
+	public static boolean containsInt(int[] zPoint, int[] lPoint, int[] rPoint) {
+		for (int i = 0; i < zPoint.length; i++)
+			if (zPoint[i] > rPoint[i] || zPoint[i] < lPoint[i])
+				return false;
+		return true;
+	}
+	
+	
+	/**
+	 * 
+	 * @param length dimension*number of bits pro dim
+ 	 * @param cycle
+	 * @param dimension
+	 * @return
+	 */
+	public static int[] createCyclicFunction(int length, int cycle, int dimension){
+		int[] function = new int[length];
+		for(int i = 0; i < function.length; i++ ){
+			function[i] = (i/cycle) % dimension;
+		}
+		return function;
+	}
+	
+	
+	/**
+	 * 
+	 * @param point
+	 * @param mask
+	 * @return
+	 */
+	public static long computeZCode(int[] point, int bitsProdim) {
+		long sfcKey = 0L;
+		long mask = 1L;
+		int dimension = point.length;
+		int[] dimensionMasks = new int[point.length];
+		// init masks
+		Arrays.fill(dimensionMasks, 1 << (bitsProdim-1));
+		// 
+		for(int i = 0, k = dimension * bitsProdim; i < dimension * bitsProdim; i++, k--){
+			// write first dim 
+			int value = point[i % dimension];
+			// extract 
+			value = value & dimensionMasks[i % dimension];
+			dimensionMasks[i % dimension] = dimensionMasks[i % dimension] >> 1;
+			if (value != 0){
+				mask = 1L << (k-1); 
+				sfcKey |= mask; // write one 
+			}
+		}
+		return sfcKey;
+	}
+	
+	
+	/**
+	 * reverse function
+	 */
+	public static int[] computePointFromZKey(long zcode, int bitsProDim,
+			int dimension) {
+		int[] point = new int[dimension];
+		Arrays.fill(point, 0);
+		for (int i = 0; i < bitsProDim* dimension; i++) {
+			int shift = i / point.length;
+			int mask = 1 << shift;
+			// test bit on position i
+			long value =  zcode & (1L << i);
+			if (value != 0) {
+				point[point.length - 1 - (i % point.length)] |= mask;
+			}
+		}
+		return point;
+	}
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * implements next point in box method
+	 *  @see H. Tropf, H. Herzog: Multidimensional Range Search in Dynamically Balanced Trees, Angewandte Informatik, 2/1981, pp 71-77
+	 *
+	 * @param zcode
+	 * @param bitsProDim number of bits which is needed to represent key
+	 * @param dimension
+	 * @return next value of z key within the query box
+	 */
+	public static long nextInBoxZValue(long zcode, int[] lPoint,
+			int[] rPoint, int bitsProDim, int dimension) {
+		long nextMatch = zcode;
+		int[] zPoint = computePointFromZKey(zcode, bitsProDim, dimension);
+		int[] indexLo = new int[dimension];
+		int[] indexHi = new int[dimension];
+		Arrays.fill(indexLo, -1);
+		Arrays.fill(indexHi, -1);
+		for (int i = 0; i < dimension; i++) { // step 2 check all Coordinates
+			if (zPoint[i] > rPoint[i]) // check right point 
+				indexHi[i] = (posCoordInt(zPoint[i], rPoint[i], bitsProDim) + 1)
+						* dimension - (i + 1);
+			if (zPoint[i] < lPoint[i]) // check left  point
+				indexLo[i] = (posCoordInt(zPoint[i], lPoint[i], bitsProDim) + 1)
+						* dimension - (i + 1);
+		}
+		int maxL = -1;// find max indexLo and indexHi
+		int maxH = -1;
+		for (int i = 0; i < dimension; i++) {
+			maxL = (indexLo[i] > maxL) ? indexLo[i] : maxL;
+			maxH = (indexHi[i] > maxH) ? indexHi[i] : maxH;
+		}
+		if (maxH > maxL) {
+			for (int i = maxH + 1; i < dimension * bitsProDim; i++) {
+				boolean testBit =  (nextMatch &(1L << i)) != 0;
+				if (!testBit
+						&& rPoint[dimension - (i % dimension) - 1] > zPoint[dimension
+								- (i % dimension) - 1]) {
+					indexLo[dimension - (i % dimension) - 1] = i;
+					break;
+				}
+			}
+		}
+		int max = -1;
+		for (int i = 0; i < dimension; i++) {// step 4
+			if (indexLo[i] > -1)
+				max = (indexLo[i] > max) ? indexLo[i] : max;
+		}
+		if (max > -1) {
+			// test
+			long flipMask = 1L << max;
+			nextMatch = nextMatch ^ flipMask;// change to 0
+			nextMatch = nextMatch >> max; // step 5
+			nextMatch = nextMatch << max;
+		}
+		for (int i = 0; i < dimension; i++) {
+			int value = readDimension(nextMatch, i, dimension, bitsProDim);
+			if (value < lPoint[i]) {
+				nextMatch = writeDimension(nextMatch, lPoint[i], i, bitsProDim,
+						dimension);
+			}
+		}
+		return nextMatch;
+	}
+
+	/**
+	 * helper method
+	 * 
+	 * @param zcode
+	 * @param dim
+	 * @param bitProDim
+	 * @param dimensions
+	 * @return
+	 */
+	private static int readDimension(long zcode, int dim, int dimensions, int bitsProdim) {
+		int[] reverseValue = computePointFromZKey(zcode, bitsProdim, dimensions);
+		int val =  reverseValue[dim];
+		return val;
+	}
+
+	/**
+	 * helper method
+	 * 
+	 * @param zcode
+	 * @param dim
+	 * @param bitsProDim
+	 * @param dimensions
+	 */
+	private static long writeDimension(long zcode, int value,
+			int dim, int bitsProDim, int dimensions) {
+		int[] reverseCode = computePointFromZKey(zcode, bitsProDim, dimensions);
+		reverseCode[dim] = value;
+		long code = computeZCode(reverseCode, bitsProDim);
+		return code;
+	}
+	
+
+	/**
+	 * help method finds highest bit position that differs (2 second step)
+	 * 
+	 * @param c1
+	 *            1-dim coordinate
+	 * @param c2
+	 *            1-dim coordinate
+	 * @param n
+	 *            resolution bits pro dim
+	 * @return 
+	 */
+	private static int posCoordInt(int c1, int c2, int n) {
+		int mask = 1 << (n - 1);
+		int i;
+		for (i = n - 1; ((mask & c1) ^ (mask & c2)) == 0 && i >= 0; i--, mask = mask >> 1)
+			;
+		return i;
+	}
+	
+	/**
+	 * Computes the list of ranges which runs through the query box for a Z-Curve. 
+	 * This is a recursive algorithms, which cuts the box  along the separation dimension (first bit which is differs)
+	 * until the whole box is within the region (equal prefix:11111...111, equal prefix:000000... 0000)    
+	 * 
+	 * @param lPoint
+	 * @param rPoint
+	 * @param bitsProDim
+	 * @param dimension
+	 * @return
+	 */
+	public static List<long[]> computeZBoxRanges(int[] lPoint,
+			int[] rPoint, int bitsProDim, int dimension ){
+		List<long[]> leftResult = null;
+		List<long[]> rightResult = null;
+		long zvalueLeft = computeZCode(lPoint, bitsProDim);
+		long zvalueRight = computeZCode(rPoint, bitsProDim);
+		long mask= 1L << 63;
+		int i;
+		for( i = 64; 
+		((mask & zvalueLeft)  == (mask & zvalueRight)) & i  >= 0;
+		i--, mask = mask >> 1 );
+		i = 64-i; // prefix length
+		boolean ones = (((1L<<(64-i))-1) & zvalueRight ) == (((1L<<(64-i))-1));
+		boolean nulls = (((1L<<(64-i))-1) & zvalueLeft ) == 0L;
+		boolean continuesSequence = ones && nulls;
+		if(continuesSequence){
+			leftResult = new LinkedList<long[]>();
+			leftResult.add(new long[]{zvalueLeft, zvalueRight});
+			return  leftResult;
+		}
+		int[] newLow = cut(zvalueLeft, zvalueRight,  bitsProDim,  dimension,  i, true);
+		int[] newHigh = cut(zvalueLeft, zvalueRight,  bitsProDim,  dimension, i, false);
+		leftResult = computeZBoxRanges(lPoint, newHigh, bitsProDim,  dimension);
+		rightResult = computeZBoxRanges(newLow, rPoint, bitsProDim,  dimension);
+		leftResult.addAll(rightResult);
+		return leftResult;
+	}
+	
+	
+	/**
+	 * helper method, which cuts the space in along the hyperplane
+	 * @param zvalueLeft
+	 * @param zvalueRight
+	 * @param bitsProDim
+	 * @param dimension
+	 * @param prefix
+	 * @param low
+	 * @return
+	 */
+	private static int[] cut(long zvalueLeft,
+			long zvalueRight, int bitsProDim, int dimension, int prefix, boolean low){
+		int position = prefix+1; // Greatest dimension on which we cut the box;
+		position =  position / dimension; // position in dimension from left to right
+		// check to which dimension belong this value
+		int dim = (prefix) % dimension;
+		long output = 0L;
+		if (low){ // compute left lower point of new box 
+			output = zvalueLeft;
+			// extract dimension 
+			int dimvalue = readDimension(output, dim, dimension, bitsProDim);
+			// set one on the position with a trailing 0 
+			int mask = 1 << (32 - position);
+			dimvalue = (dimvalue >> (32-position)) << (32-position);
+			dimvalue |=mask; // write value
+			output = writeDimension(output, dimvalue, dim, bitsProDim, dimension);
+			return computePointFromZKey(output, bitsProDim, dimension);
+		}
+		output = zvalueRight;
+		// extract dimension 
+		int dimvalue = readDimension(output, dim, dimension, bitsProDim);
+		// set 0 on the position with trailing 1;
+		int mask = (1 << (32 - position))-1;
+		dimvalue = (dimvalue >> (32-position+1)) << (32-position+1);
+		dimvalue |=mask; // write value
+		output = writeDimension(output, dimvalue, dim, bitsProDim, dimension);
+		return computePointFromZKey(output, bitsProDim, dimension);
+	}
+	
 }
