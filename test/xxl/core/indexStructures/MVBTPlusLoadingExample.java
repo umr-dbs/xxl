1ef8e21a1157ea708facf959145d5528c570f5bd
daniar.ac@gmail.com <daniar.ac@gmail.com@6cdef95d-3fa7-774b-3207-c1bd5d6c1983>
2014-02-26 14:14:50 +0000

    imports updated

--- test/xxl/core/indexStructures/MVBTPlusLoadingExample.java
+++ test/xxl/core/indexStructures/MVBTPlusLoadingExample.java
@@ -33,7 +33,7 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
-import java.util.stream.IntStream;
+//import java.util.stream.IntStream;
 
 import xxl.core.collections.containers.Container;
 import xxl.core.collections.containers.CounterContainer;
